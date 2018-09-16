0db0fa1e1424dfc684979f9a4041a5cb398ccb02
IbnuAmin <mihfazhillah@gmail.com>
2018-09-16 17:22:01 +0700

    course list at frontend

--- courses/views.py
+++ courses/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic.list import ListView
+from django.views.generic.list import ListView 
+from django.views.generic.detail import DetailView
 from django.views.generic.base import TemplateResponseMixin, View
 from django.forms.models import modelform_factory
 from django.apps import apps
+from django.db.models import Count
 
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from braces.views import LoginRequiredMixin, PermissionRequiredMixin, CsrfExemptMixin, JsonRequestResponseMixin
 
-from .models import Course, Content, Module
+from .models import Course, Content, Module, Subject
 from .forms import ModuleFormset
 
 
@@ -152,4 +154,28 @@ class ContentOrderView(CsrfExemptMixin,
             Content.objects.filter(id=id,
                                    module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules=Count('modules')
+        )
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+
+        return self.render_to_response({'subject': subject,
+                                        'subjects': subjects,
+                                        'courses': courses})
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
 
--- educa/urls.py
+++ educa/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
 
 urlpatterns = [
     # Examples:
@@ -11,4 +12,5 @@ urlpatterns = [
     url(r'^accounts/logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^courses/', include('courses.urls')),
+    url(r'^$', CourseListView.as_view(), name='course_list'),
 ]
